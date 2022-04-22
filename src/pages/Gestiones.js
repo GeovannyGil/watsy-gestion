e2533e7eb3384ad7de0eb5aaf1442d1654fbc0a0
geovannygil <geovannygr2018@gmail.com>
2022-04-22 16:15:53 -0600

    feat(Root): Add functionality form data general and generate Email

--- src/pages/Gestiones.js
+++ src/pages/Gestiones.js
@@ -1,10 +1,19 @@
 import Sidebar from '../components/SideBar/Sidebar'
 import styled from 'styled-components'
-import { TextIndicador, TitleMultiColor } from '../components/Elements/Text'
-import { Input, GroupButtons } from '../components/Elements/Inputs'
 import * as Io from 'react-icons/io'
 import * as Ai from 'react-icons/ai'
+import { TextIndicador, TitleMultiColor } from '../components/Elements/Text'
+import { Input, GroupButtons } from '../components/Elements/Inputs'
+import { ButtonOnInput, ButtonIconAlone } from '../components/Elements/Buttons'
+import { FileComponent } from '../components/Files/Files'
 import NumberFormat from 'react-number-format'
+import { v4 as uuidv4 } from 'uuid'
+// Hooks
+import { useState, useEffect } from 'react'
+
+// Toast
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const Content = styled.div`
   width: 100%;
@@ -12,30 +21,12 @@ const Content = styled.div`
 `
 
 const ContentHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
   margin-bottom: 15px;
 `
 
 const ContentBody = styled.div`
-`
-
-const ButtonOnInput = styled.button`
-  background-color: transparent;
-  border: none;
-  color: #FBFBFB;
-  font-size: 1.2em;
-  position: absolute;
-  border-radius: 0.5em;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 6px;
-  bottom: 15%;
-  right: 5%;
-  cursor: pointer;
-
-  &:hover{
-    color: #FF5F00;
-  }
 `
 
 const ContentSpaceGestion = styled.div`
@@ -60,40 +51,180 @@ const GridInputs = styled.div`
   }
 `
 
+const LayoutUpload = styled.div`
+  width: 100%;
+  height: 14rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`
+
+const Upload = styled.div`
+  width: 100%;
+  padding: 20px;
+  border: 2px dashed #FF5F00;
+  background-color: #171717;
+  border-radius: 1em;
+`
+
+const ContentFiles = styled.div`
+  padding: 0 0 0 20px;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 15px;
+  align-content: center;
+`
+
+const GroupButtonsHeaderActions = styled.div`
+  display: flex;
+  gap: 15px;
+
+  & button{
+    font-size: 1.4em;
+    padding: 5px 12px;
+  }
+`
+
 export default function Gestiones () {
+  const [dataClient, setDataClient] = useState({
+    id: uuidv4(),
+    names: '',
+    lastNames: '',
+    phone: '',
+    cui: '',
+    dateBirthday: '',
+    email: '',
+    password: ''
+  })
+
+  useEffect(() => {
+    console.log('Cambio el estado')
+  }, [])
+
+  function handleOnChange (e, label = '') {
+    const data = { ...dataClient }
+
+    if (label !== '') {
+      const jsonFormat = e
+      if (label === 'phone') {
+        data.phone = jsonFormat.value
+      }
+
+      if (label === 'cui') {
+        data.cui = jsonFormat.value
+      }
+      setDataClient(data)
+      return
+    }
+
+    if (e.target.name === 'names') {
+      data.names = e.target.value
+    }
+    if (e.target.name === 'lastNames') {
+      data.lastNames = e.target.value
+    }
+    if (e.target.name === 'cui') {
+      data.cui = e.target.value
+    }
+    if (e.target.name === 'dateBirthday') {
+      data.dateBirthday = e.target.value
+    }
+    if (e.target.name === 'email') {
+      data.email = e.target.value
+    }
+    if (e.target.name === 'password') {
+      data.password = e.target.value
+    }
+    setDataClient(data)
+  }
+
+  // function handleOnSubmit () {}
+  function handleOnCopy (textCopy) {
+    const text = dataClient[textCopy]
+    navigator.clipboard.writeText(text)
+    // Colocar un toast de que se copio el texto
+    // https://fkhadra.github.io/react-toastify/installation
+    // https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
+    toast.success('Se copio al portapapeles', {
+      position: 'bottom-center',
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'dark'
+    })
+
+    console.log('Texto Copiado')
+  }
+
+  function GenerateMail () {
+    const data = { ...dataClient }
+
+    let emailGenerate = ''
+    const dataName = dataClient.names.replace(/ /g, '').toLowerCase()
+    const dataLastName = dataClient.lastNames.replace(/ /g, '').toLowerCase()
+    const emailFormat = removeAccents(`${dataName}${dataLastName}@gmail.com`)
+    emailGenerate = emailFormat
+
+    data.email = emailGenerate
+    setDataClient(data)
+    // Email
+    // Geovanny Gil
+    // geovannygil@gmail.com
+    // geovannygil
+    // Contraseña
+    // 4 digitos de cui
+    // @
+    // primer nombre, mayuscula la primera letra
+  }
+
+  const removeAccents = (str) => {
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+  }
+
   return (
     <Sidebar>
       <Content>
         <ContentSpaceGestion>
           <ContentHeader>
             <TitleMultiColor textNormal='Crear' textColorized='Gestiones.' />
+            <GroupButtonsHeaderActions>
+              <ButtonIconAlone>
+                <Io.IoMdShare />
+              </ButtonIconAlone>
+              <ButtonIconAlone>
+                <Io.IoMdSave />
+              </ButtonIconAlone>
+            </GroupButtonsHeaderActions>
           </ContentHeader>
           <ContentBody>
-            <TextIndicador text='Datos Generales' />
+            <TextIndicador text='Datos Generales' my='10px' />
             <Input marginBottom='15px'>
               <GroupButtons>
-                <button><Io.IoMdCopy /></button>
+                <button onClick={() => handleOnCopy('names')}><Io.IoMdCopy /></button>
               </GroupButtons>
               <label>Nombres</label>
-              <input type='text' />
+              <input type='text' name='names' onChange={handleOnChange} />
             </Input>
             <GridInputs>
               <GroupGridContentDouble>
                 <Input>
                   <GroupButtons>
-                    <button><Io.IoMdCopy /></button>
+                    <button onClick={() => handleOnCopy('lastNames')}><Io.IoMdCopy /></button>
                   </GroupButtons>
                   <label>Apellidos</label>
-                  <input type='text' />
+                  <input type='text' name='lastNames' onChange={handleOnChange} />
                 </Input>
                 <Input>
                   <GroupButtons>
-                    <button><Io.IoMdCopy /></button>
+                    <button onClick={() => handleOnCopy('phone')}><Io.IoMdCopy /></button>
                   </GroupButtons>
                   <label>Teléfono</label>
                   <NumberFormat
                     type='text'
-                    name='phoneClient'
+                    name='phone'
+                    onValueChange={(e) => handleOnChange(e, 'phone')}
                     format='+(502) ####-####'
                     allowEmptyFormatting
                     mask='_'
@@ -103,12 +234,13 @@ export default function Gestiones () {
               <GroupGridContentDouble>
                 <Input>
                   <GroupButtons>
-                    <button><Io.IoMdCopy /></button>
+                    <button onClick={() => handleOnCopy('cui')}><Io.IoMdCopy /></button>
                   </GroupButtons>
                   <label>CUI</label>
                   <NumberFormat
                     type='text'
-                    name='phoneClient'
+                    name='cui'
+                    onValueChange={(e) => handleOnChange(e, 'cui')}
                     format='#### ##### ####'
                     allowEmptyFormatting
                     mask='-'
@@ -116,12 +248,13 @@ export default function Gestiones () {
                 </Input>
                 <Input>
                   <GroupButtons>
-                    <button><Io.IoMdCopy /></button>
+                    <button onClick={() => handleOnCopy('dateBirthday')}><Io.IoMdCopy /></button>
                   </GroupButtons>
                   <label>Fecha de nacimiento</label>
                   <NumberFormat
                     type='text'
-                    name='phoneClient'
+                    name='dateBirthday'
+                    onChange={handleOnChange}
                     format='##/##/####'
                     allowEmptyFormatting
                     mask={['d', 'd', 'M', 'M', 'y', 'y', 'y', 'y']}
@@ -132,25 +265,33 @@ export default function Gestiones () {
                 <Input>
                   <GroupButtons>
                     <button><Ai.AiFillCheckCircle /></button>
-                    <button><Ai.AiOutlineReload /></button>
-                    <button><Io.IoMdCopy /></button>
+                    <button onClick={GenerateMail}><Ai.AiOutlineReload /></button>
+                    <button onClick={() => handleOnCopy('email')}><Io.IoMdCopy /></button>
                   </GroupButtons>
                   <label>Correo Electrónico</label>
-                  <input tyep='email' />
+                  <input tyep='email' value={dataClient.email} name='email' onChange={handleOnChange} />
                 </Input>
                 <Input>
                   <GroupButtons>
                     <button><Ai.AiOutlineReload /></button>
-                    <button><Io.IoMdCopy /></button>
+                    <button onClick={() => handleOnCopy('password')}><Io.IoMdCopy /></button>
                   </GroupButtons>
                   <label>Contraseña</label>
-                  <input type='password' />
+                  <input type='password' name='password' onChange={handleOnChange} />
                   <ButtonOnInput>
                     <Ai.AiFillEyeInvisible />
                   </ButtonOnInput>
                 </Input>
               </GroupGridContentDouble>
             </GridInputs>
+            <TextIndicador text='Archivos Adjuntos' my='15px' />
+            <LayoutUpload>
+              <Upload />
+              <ContentFiles>
+                <FileComponent />
+                <FileComponent />
+              </ContentFiles>
+            </LayoutUpload>
           </ContentBody>
         </ContentSpaceGestion>
       </Content>
