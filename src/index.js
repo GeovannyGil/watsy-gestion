a705ee435ffb56a12735ec836659088fdb59767a
geovannygil <geovannygr2018@gmail.com>
2022-04-22 16:15:02 -0600

    feat(Root): Add toast in index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 import Gestiones from './pages/Gestiones'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
+    <ToastContainer />
     <Routes>
       <Route path='/' element={<App />} />
       <Route path='/gestiones' element={<Gestiones />} />
